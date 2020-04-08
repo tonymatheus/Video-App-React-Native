750dd0b367bba19a35d56ec1e288627224209f00
fsegall <segall.felipe@gmail.com>
2020-04-08 16:09:50 -0300

    Seta state para esconder componentes em fullscreen

--- src/VideoApp.js
+++ src/VideoApp.js
@@ -1,16 +1,24 @@
 import React, {Component} from 'react';
 import {Text, View, StyleSheet} from 'react-native';
 import VideoPlayer from 'react-native-video-controls';
-import Video from 'react-native-video';
+// import Video from 'react-native-video';
 
 export default class videoApp extends Component {
+  state = {fullscreen: false};
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.navbar}>
-          <Text style={styles.headerText}>Senado News</Text>
-        </View>
+        {!this.state.fullscreen && (
+          <View style={styles.navbar}>
+            <Text style={styles.headerText}>Senado News</Text>
+          </View>
+        )}
         <VideoPlayer
+          onEnterFullscreen={() =>
+            this.setState({
+              fullscreen: !this.state.fullscreen,
+            })
+          }
           source={{uri: 'https://vjs.zencdn.net/v/oceans.mp4'}}
           navigator={this.props.navigator}
         />
